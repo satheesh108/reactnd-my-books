bb2abd16a2ad463c74fb4932a86a13364ed47bb9
Satheesh Kalyanaraman <satheeshkalyanaraman@gmail.com>
2017-12-12 18:13:12 -0500

    BookStore Beta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import ListBooks from './ListBooks';
+import * as BooksAPI from './BooksAPI';
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -10,10 +12,34 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: [],
+    shelfs: ['currentlyReading', 'read', 'wantToRead']
   }
-
+  componentDidMount(){
+    BooksAPI.getAll().then((books) =>{
+      this.setState({books})
+      console.dir(books);
+    })
+  }
   render() {
+    //Here goes the actual render
+    return(
+      <div className='app'>
+          <div className="list-books">
+              <div className="list-books-title">
+                <h1>My Reads</h1>
+              </div>
+              {this.state.shelfs.map((shelf)=>(
+                <ListBooks
+                    books={this.state.books.filter((book) => (book.shelf.trim() === shelf.trim()))}
+                    shelf={shelf}/>
+              ))}
+          </div>
+      </div>
+    )
+  }
+  Testrender() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
